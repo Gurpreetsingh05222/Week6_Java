4cd0c3bd79808b37a85a1291d81e1ef3f265c02a
gurpreetsingh05222 <gurpreetsingh05222@gmail.com>
2021-02-27 09:49:35 -0500

    student class with all attributes

--- src/com/gurpreet/Student.java
+++ src/com/gurpreet/Student.java
@@ -1,4 +1,22 @@
 package com.gurpreet;
 
+import java.util.Date;
+
 public class Student {
+    private int studentId;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private Gender gender;
+    private String address;
+    private String city;
+    private String pinCode;
+    private String cellPhone;
+    private String email;
+    private String department;
+    private float marks[];
+    private float totalMarks;
+    private float percentage;
+    private String grade;
+
 }
