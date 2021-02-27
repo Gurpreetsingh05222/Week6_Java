e2f9ca2753ac65edf301f3488d13bf735cae3d75
gurpreetsingh05222 <gurpreetsingh05222@gmail.com>
2021-02-27 09:38:51 -0500

    Student class added

---
+++ src/com/gurpreet/Student.java
@@ -0,0 +1,4 @@
+package com.gurpreet;
+
+public class Student {
+}
--- src/com/gurpreet/Week6.java
+++ src/com/gurpreet/Week6.java
@@ -2,6 +2,6 @@ package com.gurpreet;
 
 public class Week6 {
     public static void main(String[] args){
-
+        System.out.println("Hello World!");
     }
 }
